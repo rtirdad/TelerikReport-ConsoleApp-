ac4ce0634ae7bc4ccf4982a0818d7661d541a969
rtirdad <94053847+rtirdad@users.noreply.github.com>
2023-12-04 12:42:06 +0100

    Changed Report name

--- TelerikReport/Program.cs
+++ TelerikReport/Program.cs
@@ -6,6 +6,7 @@ using Telerik.Reporting;
 using Telerik.Reporting.Processing;
 using Newtonsoft.Json;
 using TelerikReport;
+using System.ComponentModel.Composition.Primitives;
 
 namespace TelerikReportingDemo
 {
@@ -36,25 +37,72 @@ namespace TelerikReportingDemo
 
             var data = Newtonsoft.Json.JsonConvert.DeserializeObject<List<dynamic>>(jsonData);
 
-            Report1 report = new Report1();
-            report.DataSource = data;
+            Report1 reportName = new Report1();
+            reportName.DataSource = data;
 
 
-            ReportProcessor reportProcessor = new ReportProcessor();
+            /*ReportProcessor reportProcessor = new ReportProcessor();
             RenderingResult result = reportProcessor.RenderReport("PDF", report, null);
 
             if (result.DocumentBytes != null && result.DocumentBytes.Length > 0)
             {
                 //string outputPath = "output/path/output.pdf";
-                string outputPath = "c#/TelerikReport/output";
+                string outputPath = "c#/TelerikReport/output.";
                 System.IO.File.WriteAllBytes(outputPath, result.DocumentBytes);
                 Console.WriteLine("PDF generated successfully!");
             }
             else
             {
                 Console.WriteLine("Error: No document bytes generated.");
-            }
+            }*/
+            var reportProcessor = new Telerik.Reporting.Processing.ReportProcessor();
 
+            // set any deviceInfo settings if necessary
+            var deviceInfo = new System.Collections.Hashtable();
+
+            // Depending on the report definition choose ONE of the following REPORT SOURCES
+            //                  -1-
+            // ***CLR (CSharp) report definitions***
+            var reportSource = new Telerik.Reporting.TypeReportSource();
+
+            // reportName is the Assembly Qualified Name of the report
+            //reportSource.TypeName = reportName;
+            //                  -1-
+
+            ////                  -2-
+            //// ***Declarative (TRDP/TRDX) report definitions***
+            //var reportSource = new Telerik.Reporting.UriReportSource();
+
+            //// reportName is the path to the TRDP/TRDX file
+            //reportSource.Uri = reportName;
+            ////                  -2-
+
+            ////                  -3-
+            //// ***Instance of the report definition***
+            //var reportSource = new Telerik.Reporting.InstanceReportSource();
+
+            //// Report1 is the class of the report. It should inherit Telerik.Reporting.Report class
+            //reportSource.ReportDocument = new Report1();
+            ////                  -3-
+
+            // Pass parameter value with the Report Source if necessary
+            object parameterValue = "Some Parameter Value";
+            reportSource.Parameters.Add("ParameterName", parameterValue);
+
+            //Telerik.Reporting.Processing.RenderingResult result = reportProcessor.RenderReport("PDF", reportSource, deviceInfo);
+            RenderingResult result = reportProcessor.RenderReport("PDF", reportName, null);
+
+            if (!result.HasErrors)
+            {
+                string fileName = result.DocumentName + "." + result.Extension;
+                string path = System.IO.Path.GetTempPath();
+                string filePath = System.IO.Path.Combine(path, fileName);
+
+                using (System.IO.FileStream fs = new System.IO.FileStream(filePath, System.IO.FileMode.Create))
+                {
+                    fs.Write(result.DocumentBytes, 0, result.DocumentBytes.Length);
+                }
+            }
         }
     }
 }
