9828c949233ffe9668a368af2e20e5768dd6883b
rtirdad <94053847+rtirdad@users.noreply.github.com>
2023-12-04 11:41:09 +0100

    Json input added

--- TelerikReport/Program.cs
+++ TelerikReport/Program.cs
@@ -1,42 +1,60 @@
-﻿using System;
-using System.Collections;
+﻿
+using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using Telerik.Reporting;
 using Telerik.Reporting.Processing;
+using Newtonsoft.Json;
+using TelerikReport;
 
-namespace ConsoleApp2101
+namespace TelerikReportingDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var reportSource = new UriReportSource();
-            var processor = new ReportProcessor();
-            var deviceInfo = new Hashtable();
+            var jsonData = @"[
+                {
+                    ""store"": {
+                        ""book"": [
+                            {
+                                ""category"": ""reference"",
+                                ""author"": ""Nigel Rees"",
+                                ""title"": ""Sayings of the Century"",
+                                ""price"": 8.95
+                            },
+                            {
+                                ""category"": ""fiction"",
+                                ""author"": ""Evelyn Waugh"",
+                                ""title"": ""Sword of Honour"",
+                                ""price"": 12.99
+                            }
+                        ]
+                    }
+                }
+            ]";
 
-            reportSource.Uri = @"C:\Program Files (x86)\Progress\Telerik Reporting R3 2023\Report Designer\Examples\MyReport.trdp";
-            
-            deviceInfo.Add("DocumentTitle", "SomeOptionalTitle");
+            var data = Newtonsoft.Json.JsonConvert.DeserializeObject<List<dynamic>>(jsonData);
 
-            string[] availableFormats = new string[] { "PDF", "CSV", "DOCX", "XLSX", "PPTX", "RTF" };
-            
-            foreach (var format in availableFormats)
+            Report1 report = new Report1();
+            report.DataSource = data;
+
+
+            ReportProcessor reportProcessor = new ReportProcessor();
+            RenderingResult result = reportProcessor.RenderReport("PDF", report, null);
+
+            if (result.DocumentBytes != null && result.DocumentBytes.Length > 0)
             {
-                var result = processor.RenderReport(format, reportSource, deviceInfo);
-
-                if (result.HasErrors)
-                {
-                    Console.WriteLine(string.Join(",", result.Errors.Select(s => s.Message)));
-                }
-                else
-                {
-                    File.WriteAllBytes($"MyReport.{format.ToLower()}", result.DocumentBytes);
-                }
+                //string outputPath = "output/path/output.pdf";
+                string outputPath = "c#/TelerikReport/output";
+                System.IO.File.WriteAllBytes(outputPath, result.DocumentBytes);
+                Console.WriteLine("PDF generated successfully!");
             }
-            
-            Console.WriteLine("Completed!");
-            Console.ReadKey();
+            else
+            {
+                Console.WriteLine("Error: No document bytes generated.");
+            }
+
         }
     }
 }
