6bdcabe4493fbc7d82230119c04588a8f0cbdc0f
rtirdad <94053847+rtirdad@users.noreply.github.com>
2023-12-05 12:59:42 +0100

    .

--- TelerikReport/Program.cs
+++ TelerikReport/Program.cs
@@ -6,7 +6,6 @@ using Telerik.Reporting;
 using Telerik.Reporting.Processing;
 using TelerikReport;
 
-
 namespace TelerikReportingDemo
 {
     class Program
@@ -31,7 +30,7 @@ namespace TelerikReportingDemo
                                 ""postalZip"": ""13732"",
                                 ""salary"": 5000
                             },
-  {
+                            {
                                 ""name"": ""Angelica Salinas"",
                                 ""country"": ""France"",
                                 ""region"": ""Lambayeque"",
@@ -50,46 +49,22 @@ namespace TelerikReportingDemo
             report.SkipBlankPages = false;
 
             Telerik.Reporting.PageHeaderSection Header1 = new PageHeaderSection();
-            Header1.Name = "$.company.employees.name[?(@.salary=4000)]";
-            report.Items.Add(Header1);
 
-
-            /*Header1.Style.BorderStyle.Default = Telerik.Reporting.Drawing.BorderType.Solid;
-            var numberTextBox = new Telerik.Reporting.TextBox();
-
-            numberTextBox.Value = "$.company.employee.name[?(@.salary=4000)]";
-            numberTextBox.Left = Telerik.Reporting.Drawing.Unit.Inch(0);
-            numberTextBox.Top = Telerik.Reporting.Drawing.Unit.Inch(0);
-            numberTextBox.Width = Telerik.Reporting.Drawing.Unit.Inch(2);
-            numberTextBox.Height = Telerik.Reporting.Drawing.Unit.Inch(0.2);
-            Header1.Items.Add(numberTextBox);
-            report.Items.Add(Header1);
-
+            var headerTextBox = new Telerik.Reporting.TextBox();
+            headerTextBox.Value = "=Fields.company.employees[0].name";
+            headerTextBox.Left = Telerik.Reporting.Drawing.Unit.Inch(0);
+            headerTextBox.Top = Telerik.Reporting.Drawing.Unit.Inch(0);
+            headerTextBox.Width = Telerik.Reporting.Drawing.Unit.Inch(2);
+            headerTextBox.Height = Telerik.Reporting.Drawing.Unit.Inch(0.2);
+            Header1.Items.Add(headerTextBox);
+            //report.Items.Add(Header1);
 
             Telerik.Reporting.DetailSection Detail1 = new Telerik.Reporting.DetailSection();
-            Detail1.Name = "detail1";
-            report.Items.Add((Telerik.Reporting.ReportItemBase)Detail1);
-            report.Items.Add(Detail1);
 
-            report.Width = Telerik.Reporting.Drawing.Unit.Inch(4);
+            Telerik.Reporting.PageFooterSection footer = new PageFooterSection();
 
-            var Header1 = new Telerik.Reporting.PageHeaderSection();
 
-            Header1.Height = Telerik.Reporting.Drawing.Unit.Inch(0.2);
-            report.Items.Add(Header1);
-
-            var numberTextBox = new Telerik.Reporting.TextBox();
-
-            numberTextBox.Value = "$.company.employee[?(@.salary=4000)]";
-            numberTextBox.Left = Telerik.Reporting.Drawing.Unit.Inch(0);
-            numberTextBox.Top = Telerik.Reporting.Drawing.Unit.Inch(0);
-            numberTextBox.Width = Telerik.Reporting.Drawing.Unit.Inch(2);
-            numberTextBox.Height = Telerik.Reporting.Drawing.Unit.Inch(0.2);
-            Header1.Items.Add(numberTextBox);*/
-
-            Telerik.Reporting.PageFooterSection FooterSection = new PageFooterSection();
-          
-
+            
             var reportProcessor = new ReportProcessor();
 
             var reportSource = new Telerik.Reporting.InstanceReportSource();
@@ -99,16 +74,16 @@ namespace TelerikReportingDemo
 
             if (!result.HasErrors)
             {
-                string fileName = "Report1.pdf";
+                string fileName = "Report.pdf";
                 string path = "C:\\Users\\CityGIS\\Desktop\\C#\\TelerikReport\\SavedPFD";
-                string filePath = System.IO.Path.Combine(path, fileName);
+                string filePath = Path.Combine(path, fileName);
 
-                using (System.IO.FileStream fs = new System.IO.FileStream(filePath, System.IO.FileMode.Create))
+                using (FileStream fs = new FileStream(filePath, FileMode.Create))
                 {
                     fs.Write(result.DocumentBytes, 0, result.DocumentBytes.Length);
                 }
 
-                Console.WriteLine("PDF is saved :)");
+                Console.WriteLine("PDF has successfully saved :)");
             }
             else
             {
